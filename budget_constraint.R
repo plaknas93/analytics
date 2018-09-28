ff94fd664bf2abee92e0a3977c85a2d5f180cde9
Sankalp <mail2plaknas@gmail.com>
2018-09-28 08:56:09 +0530

    new commit

--- budget_constraint.R
+++ budget_constraint.R
@@ -16,4 +16,29 @@ max_bread=income/bread_price
 ?curve()
 curve(income/wine_price - ((bread_price*x)/wine_price),from = 0, to = max_bread,xlab = "Qty of Bread (slice)",ylab = "Qty of Wine (oz)",col="red", xaxs="i", yaxs='i')
 
+#Plotting Demand Curve
+#Function: Qd = 180 - 2*P => P = 90 - 0.5*Qd
+curve(90 - (0.5*x),from = 0, to = 180,xlab = "Qty",ylab = "Price",col="red", xaxs="i", yaxs='i')
 
+#Plotting Supply Curve
+#Function: Qs = -15 + P => P = 15 + Qs
+curve(15 + x,from = 0, to = ,xlab = "Qty",ylab = "Price",col="red", xaxs="i", yaxs='i')
+
+#Both Supply and Demand together
+
+##Define variables
+x= seq(from= 0, to=180, 10)
+x
+p1 = 90 - (0.5*x)
+p2 = 15 + x
+p2
+
+### Method 1
+matplot(x, cbind(p1,p2),type="l",col=c("red","green"),lty=c(1,1), xlab = "Quantity", ylab="Price")
+        
+### Method 2 : ggplot
+df= data.frame(x,p1,p2)
+library(ggplot2)
+ggplot(df, aes(x)) +                    # basic graphical object
+    geom_line(aes(y=p1), colour="red") +  # first layer
+    geom_line(aes(y=p2), colour="green")  # second layer
